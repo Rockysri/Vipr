24a710c204693c8d8ff050bf02ef0dd9bd92ae77
rocky-557 <raghavsri14@gmail.com>
2023-12-04 19:35:41 +0530

    hi

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,20 @@
-body.onload = (event)=>{
+
+window.onload = (event)=>{
 alert("hi");
-};
+}
 console.log("hi");
+
+var socket = io();
+
+
+function getinp(){
+    var uname= document.getElementById('uname').value;
+var pass1= document.getElementById('pass').value;
+    console.log(uname + " " + pass1);
+    var udetails=[uname,pass1];
+    console.log(udetails);
+    socket.emit("login",[uname,pass1]);
+    document.getElementById("box1").innerHTML='<object type="text/html" data="android_large_2.html" ></object>';
+    
+}
+
--- server.js
+++ server.js
@@ -6,16 +6,22 @@ const server = app.listen(PORT,()=>{
 })
 const io = require("socket.io")(server);
 
-app.use(express.static(__dirname+"/public"));
-console.log(__dirname+"/public")
+app.use(express.static(__dirname+"/docs"));
 
+var users =[];
 io.on("connection",(socket)=>{
     console.log(socket.id+" connected");
     socket.on("disconnect",()=> {
-        console.log(socket.id+" disconnected...")
+        console.log(socket.id+" disconnected...");
+        
     });
+    socket.on("login",(udetails)=>{
+        users.push(udetails);
+        console.log(users);
+    });
+    
 });
 
-io.emit("mesg");
+
 
 
